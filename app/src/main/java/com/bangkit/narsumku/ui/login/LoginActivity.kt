6d06d74d4b705f6c9e27d92ac533afebb2d2c2ce
Rahma Bintang Pratama <rahmabintangp@gmail.com>
2024-06-05 11:50:04 +0700

    Test Animation and Application Flow

--- app/src/main/java/com/bangkit/narsumku/ui/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/narsumku/ui/login/LoginActivity.kt
@@ -1,12 +1,103 @@
 package com.bangkit.narsumku.ui.login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import com.bangkit.narsumku.R
+import com.bangkit.narsumku.data.pref.UserModel
+import com.bangkit.narsumku.databinding.ActivityLoginBinding
+import com.bangkit.narsumku.ui.ViewModelFactory
+import com.bangkit.narsumku.ui.main.MainActivity
+import com.bangkit.narsumku.ui.signup.SignupActivity
+import com.bangkit.narsumku.ui.welcome.WelcomeActivity
 
 class LoginActivity : AppCompatActivity() {
+    private val viewModel by viewModels<LoginViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
+        playAnimation()
     }
+
+    private fun playAnimation() {
+
+        val login = ObjectAnimator.ofFloat(binding.btnLogin, View.ALPHA, 1f).setDuration(250)
+        val welcome = ObjectAnimator.ofFloat(binding.tvWelcome, View.ALPHA, 1f).setDuration(250)
+        val intro = ObjectAnimator.ofFloat(binding.tvAppIntro, View.ALPHA, 1f).setDuration(250)
+        val tvEmail = ObjectAnimator.ofFloat(binding.tvEmailOrMobileNumber, View.ALPHA, 1f).setDuration(250)
+        val email = ObjectAnimator.ofFloat(binding.EmailEditText, View.ALPHA, 1f).setDuration(250)
+        val layoutEmail = ObjectAnimator.ofFloat(binding.EmailEditTextLayout, View.ALPHA, 1f).setDuration(250)
+        val tvPassword = ObjectAnimator.ofFloat(binding.tvPassword, View.ALPHA, 1f).setDuration(250)
+        val password = ObjectAnimator.ofFloat(binding.PasswordEditText, View.ALPHA, 1f).setDuration(250)
+        val layoutPassword = ObjectAnimator.ofFloat(binding.PasswordEditTextLayout, View.ALPHA, 1f).setDuration(250)
+        val forgotPassword = ObjectAnimator.ofFloat(binding.tvForgetPassword, View.ALPHA, 1f).setDuration(250)
+        val account = ObjectAnimator.ofFloat(binding.tvNoAccount, View.ALPHA, 1f).setDuration(250)
+        val signup = ObjectAnimator.ofFloat(binding.btnSignup, View.ALPHA, 1f).setDuration(250)
+
+        val together = AnimatorSet().apply {
+            playTogether(account, signup)
+        }
+        AnimatorSet().apply {
+            playSequentially(
+                welcome,
+                intro,
+                tvEmail,
+                email,
+                layoutEmail,
+                tvPassword,
+                password,
+                layoutPassword,
+                forgotPassword,
+                login,
+                together
+            )
+            start()
+        }
+    }
+
+    private fun setupAction() {
+        binding.btnLogin.setOnClickListener {
+            val email = binding.EmailEditText.text.toString()
+            viewModel.saveSession(UserModel(email, "sample_token"))
+            AlertDialog.Builder(this).apply {
+                setTitle("Yeah!")
+                setMessage("You have successfully logged in.")
+                setPositiveButton("Continue") { _, _ ->
+                    val intent = Intent(context, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                    finish()
+                }
+                create()
+                show()
+            }
+        }
+
+        binding.btnSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, WelcomeActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+    }
+
 }
--- app/src/main/java/com/bangkit/narsumku/ui/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/narsumku/ui/main/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.bangkit.narsumku.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bangkit.narsumku.R
-import androidx.appcompat.widget.SearchView;
+import androidx.activity.viewModels
+import com.bangkit.narsumku.databinding.ActivityMainBinding
+import com.bangkit.narsumku.ui.ViewModelFactory
+import com.bangkit.narsumku.ui.welcome.WelcomeActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.getSession().observe(this) { user ->
+            if (!user.isLogin) {
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+            }
+        }
+
+        setupAction()
     }
+
+    private fun setupAction() {
+        binding.btnLogout.setOnClickListener {
+            viewModel.logout()
+        }
+    }
+
 }
